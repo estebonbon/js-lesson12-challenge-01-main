c06705579fd314b6ca38c200c56c8be9974e0b07
estebonbon <estesarmiento1@gmail.com>
2024-02-05 17:05:09 -0500

    Added an Event Listener to the button, it now shows a random image

--- js/script.js
+++ js/script.js
@@ -8,7 +8,33 @@ const getImage = async function () {
 
     const images = await res.json();
 
-    console.log(images);
+    selectRandomImage(images); // The images argument here comes from the res fetch URL values. 
+
+    //console.log(images);
 };
 
-getImage();
+
+
+const selectRandomImage = function (images) { // The images value, comes from whatever key value properties where passed onto selectRandomImage.
+    const randomIndex = Math.floor(Math.random() * images.length); // The math.floor function round the interger to the nearest whole number.
+
+    const randomImage = images[randomIndex]; // randomIndex has an integer value, images is an array of multiple images, by putting randomIndex in the brackets we a getting on random image from that array.
+
+    console.log(randomImage);
+    displayImage(randomImage); // This line of code is the link that will allow the function displayImage to take the argument and extract the key values that are containned inside it.
+};
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author // remember to use dote notation to access specific informtation
+    const imageAddress = randomImage.download_url // Again here we are using dot notation to extract a specific key-value of the randomImage
+
+    authorSpan.innerText = author; // Here we are beginning to modify the HTML 
+    img.src = imageAddress; // this is linking the random images url, to the image that will be displayed on the website.
+
+    imgDiv.classList.remove("hide");
+
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
